e9dd3fa8cfc59e715cb3fd8168d5ff13d66246ea
Jefferson Campos <38763942+JeffersonJr@users.noreply.github.com>
2020-08-07 00:10:35 -0300

    Adicionando AdminLTE
    >< putavida

--- app/Views/index.php
+++ app/Views/index.php
@@ -17,8 +17,8 @@
     <link rel="stylesheet" href="src/css/magnific-popup.css">
     <link rel="stylesheet" href="src/css/style.css">
 
-    <!-- Theme style -->
-    <link rel="stylesheet" href="src/adminlte/css/adminlte.min.css">
+    <!-- Theme style
+    <link rel="stylesheet" href="src/adminlte/css/adminlte.min.css">-->
 
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
